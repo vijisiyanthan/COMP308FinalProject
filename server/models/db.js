8f2856628564c4d081d3d40e2cb5ad8a17877acd
[Siyanthan] <[vijisiyanthan@gmail.com]>
2019-04-04 20:56:11 -0400

    Express and Mongo Db connection

---
+++ server/models/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (!err) {
+        console.log('MongoDB connection succeeded.');
+    } else {
+        console.log('Error in MongDb connection: ' + JSON.stringify(err, undefined, 2));
+    }
+});
