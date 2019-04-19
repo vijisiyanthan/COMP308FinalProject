73e7e429a555dffcad835ba54c89d2bb0473b2ae
[Siyanthan] <[vijisiyanthan@gmail.com]>
2019-04-18 20:03:41 -0400

    Update user.service.ts

--- interface/src/app/services/user.service.ts
+++ interface/src/app/services/user.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
   private endpoint = "http://localhost:3000/api/nurse/patients";
 
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept"
     })
   };
 
-   //Constructor
-  constructor(private http: HttpClient) { }
+  //NoAuth Header
+  noAuthHeader = { headers: new HttpHeaders({ NoAuth: "True" }) };
+
+  //Constructor
+  constructor(private http: HttpClient) {}
 
   //method to get list of patients
   public getList(): Observable<any> {
     return this.http.get<any>(this.endpoint, this.httpOptions);
+  }
+
+  //login
+  login(authCredentials) {
+    return this.http.post(
+      environment.apiBaseUrl + "/authenticate",
+      authCredentials,
+      this.noAuthHeader
+    );
+  }
+
+  setToken(token: string) {
+    localStorage.setItem("token", token);
+  }
+
+  getToken() {
+    return localStorage.getItem("token");
+  }
+
+  deleteToken() {
+    localStorage.removeItem("token");
+  }
+
+  getUserPayload() {
+    var token = this.getToken();
+    if (token) {
+      var userPayload = atob(token.split(".")[1]);
+      return JSON.parse(userPayload);
+    } else {
+      return null;
+    }
+  }
+  isLoggedIn() {
+    var userPayload = this.getUserPayload();
+    if (userPayload) return userPayload.exp > Date.now() / 1000;
+    else return false;
   }
 }
