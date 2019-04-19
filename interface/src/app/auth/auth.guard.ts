85c60086f953f2a431e7860a8a36d1da16748d66
[Siyanthan] <[vijisiyanthan@gmail.com]>
2019-04-18 20:04:18 -0400

    Create auth.guard.ts

---
+++ interface/src/app/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  ActivatedRoute
+} from "@angular/router";
+import { Observable } from "rxjs";
+
+import { UserService } from "src/app/services/user.service";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const expectedUser = route.data.expectedRole;
+    if (
+      !this.userService.isLoggedIn() ||
+      this.userService.getUserPayload().role !== expectedUser
+    ) {
+      this.router.navigateByUrl("/login");
+      this.userService.deleteToken();
+      return false;
+    }
+    return true;
+  }
+}
