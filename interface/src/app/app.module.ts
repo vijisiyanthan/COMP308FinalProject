943c5367ac06cf56fb674de27ad4111eae90a1fb
[Siyanthan] <[vijisiyanthan@gmail.com]>
2019-04-18 20:04:37 -0400

    Update app.module.ts

--- interface/src/app/app.module.ts
+++ interface/src/app/app.module.ts
@@ -1,23 +1,30 @@
 //Modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 //Components
-import { AppComponent } from './app.component';
-import { NurseComponent } from './pages/nurse/nurse.component';
-import { PatientComponent } from './pages/patient/patient.component';
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-
+import { AppComponent } from "./app.component";
+import { NurseComponent } from "./pages/nurse/nurse.component";
+import { PatientComponent } from "./pages/patient/patient.component";
+import { HeaderComponent } from "./partials/header/header.component";
+import { FooterComponent } from "./partials/footer/footer.component";
+import { SignupComponent } from "./pages/signup/signup.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 //Services
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import {
+  FlashMessagesModule,
+  FlashMessagesService
+} from "angular2-flash-messages";
+
+//Authorization
+import { AuthGuard } from "./auth/auth.guard";
+import { AuthInterceptor } from "./auth/auth.interceptor";
+import { UserService } from "./services/user.service";
 
 @NgModule({
   declarations: [
@@ -37,7 +44,16 @@ import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messag
     FormsModule,
     FlashMessagesModule
   ],
-  providers: [FlashMessagesService],
+  providers: [
+    FlashMessagesService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    AuthGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
